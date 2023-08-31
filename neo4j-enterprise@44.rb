6a19c0c12b2b93c75bfe5608d12962ea1909c439
Vince Bickers <vince.bickers@gmail.com>
2023-08-31 14:57:33 +0200

    Update neo4j-enterprise@44.rb
    Remove plist for Mac OS service

--- neo4j-enterprise@44.rb
+++ neo4j-enterprise@44.rb
@@ -31,35 +31,6 @@ class Neo4jEnterpriseAT44 < Formula
     (var/"log").mkpath
   end
 
-  service.require_root :manual => "neo4j start"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
-        <false/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/neo4j</string>
-          <string>console</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{var}</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/neo4j.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/neo4j.log</string>
-      </dict>
-    </plist>
-  EOS
-  end
-
   test do
     ENV["NEO4J_HOME"] = libexec
     ENV["NEO4J_LOG"] = testpath/"libexec/data/log/neo4j.log"
